6e03574f0a42e4f5cdd98833fc66579022ca5336
ganesh <ganeshnalegave1999@gmail.com>
2025-04-27 13:32:05 +0530

    Patch Method Added

--- DocumentAPI/Program.cs
+++ DocumentAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Service registration for DbContext
 builder.Services.AddDbContext<AppDbContext>(options=>
