0b12bdde05d215bab3a44fd38875b3dbb238c7d6
ganesh <ganeshnalegave1999@gmail.com>
2025-05-07 14:56:40 +0530

    Minimal API code Added

--- DocumentAPI/Program.cs
+++ DocumentAPI/Program.cs
@@ -1,5 +1,8 @@
 using DocumentAPI.Data;
+using DocumentAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Minimal Web API code snippet for Get, Post & Put.
+List<Documents> listdoc = new();
+
+app.MapGet("/GetDoc",()=> listdoc);
+
+app.MapPost("AddDoc", (Documents doc) => {
+    listdoc.Add(doc);
+    return Results.Ok(doc);
+});
+
+app.MapGet("GetDocByID", (int DocID) =>
+{
+    var doc=  listdoc.Find(u => u.DocumentID == DocID);
+    if(doc==null)
+        return Results.NotFound();
+    return Results.Ok(doc);
+});
+app.MapPut("PutDoc", (int DocID, Documents doc) =>
+{
+    var docFound = listdoc.Find(u => u.DocumentID == DocID);
+    if(docFound==null)
+        return Results.NotFound();
+    docFound.DocumentTitle=doc.DocumentTitle;
+    docFound.Documenttype = doc.Documenttype;
+    docFound.DocumentDescription = doc.DocumentDescription;
+    return Results.Ok(docFound);
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
